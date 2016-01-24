51c12728430a161ea2b645177098949e8e4aff81
ianc <ianc@amham.net>
2016-01-24 17:32:28 +0800

    Run from CDN instead of local libraries

--- public/app/js/components/d3/d3.js/d3.controllers.js
+++ public/app/js/components/d3/d3.js/d3.controllers.js
@@ -64,7 +64,7 @@ angular.module('d3', [])
     var scriptTag = $document[0].createElement('script');
     scriptTag.type = 'text/javascript';
     scriptTag.async = true;
-    scriptTag.src = 'bower_components/d3/d3.min.js';
+    scriptTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.13/d3.min.js';
     scriptTag.onreadystatechange = function () {
       if (this.readyState == 'complete') onScriptLoad();
     }
